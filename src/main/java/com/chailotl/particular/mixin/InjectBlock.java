6f74ac6e1d2a9867e28989198ef4f6f8ac3490fd
Chai <7232280+Chailotl@users.noreply.github.com>
2024-05-14 23:16:52 -0400

    Merged cave dust mixin with firefly mixin, fixing a bug with 1.20.4

--- src/main/java/com/chailotl/particular/mixin/InjectBlock.java
+++ src/main/java/com/chailotl/particular/mixin/InjectBlock.java
@@ -4,10 +4,16 @@ import com.chailotl.particular.Main;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.*;
+import net.minecraft.client.util.ParticleUtil;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LightType;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,6 +21,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Block.class)
 public class InjectBlock
 {
+	@Unique
+	private static boolean isValidBiome(RegistryEntry<Biome> biome)
+	{
+		return !biome.matchesKey(BiomeKeys.LUSH_CAVES) &&
+			!biome.matchesKey(BiomeKeys.DRIPSTONE_CAVES) &&
+			!biome.matchesKey(BiomeKeys.DEEP_DARK);
+	}
+
 	@Inject(at = @At("TAIL"), method = "randomDisplayTick")
 	public void spawnFireflies(BlockState state, World world, BlockPos pos, Random random, CallbackInfo ci)
 	{
@@ -30,6 +44,20 @@ public class InjectBlock
 			}
 
 			Main.spawnFirefly(world, pos, random);
+		}
+		else if (block == Blocks.AIR || block == Blocks.CAVE_AIR)
+		{
+			// Cave dust
+			if (random.nextInt(500) == 0 && pos.getY() < world.getSeaLevel() && isValidBiome(world.getBiome(pos)) && !state.isOf(Blocks.VOID_AIR))
+			{
+				float lightChance = 1f - Math.min(8, world.getLightLevel(LightType.SKY, pos)) / 8f;
+				float depthChance = Math.min(1f, (world.getSeaLevel() - pos.getY()) / 96f);
+
+				if (random.nextFloat() < lightChance * depthChance)
+				{
+					ParticleUtil.spawnParticle(world, pos, random, Main.CAVE_DUST);
+				}
+			}
 		}
 	}
 }
